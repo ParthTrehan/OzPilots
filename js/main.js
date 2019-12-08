8e1ae09268f95c32a9e62a7cb50a6fcc30b4aaca
Parth Trehan <parth.trehan@student.unimelb.edu.au>
2019-12-09 00:36:11 +1100

    update

--- js/main.js
+++ js/main.js
@@ -221,7 +221,7 @@ $(document).ready(function () {
         zoom: 11,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(-37.917117, 144.747690), // New York
 
         // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
@@ -237,7 +237,7 @@ $(document).ready(function () {
 
       // Let's also add a marker while we're at it
       var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(40.6700, -73.9400),
+        position: new google.maps.LatLng(-37.917117, 144.747690),
         map: map,
         title: 'Snazzy!'
       });
